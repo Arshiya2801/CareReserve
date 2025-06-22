aa4b320f07fb4d910a21a342a6f2ab4d4982e968
Arshiya Singh <singharshiya28@gmail.com>
2025-06-22 23:48:32 +0530

    fifth

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {assets} from '../assets/assets'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
+          <NavLink to='/'>
+            <li>HOME</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+          <NavLink to='/doctors'>
+            <li>ALL DOCTORS</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+          <NavLink to='/about'>
+            <li>ABOUT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+          <NavLink to='/contact'>
+            <li>CONTACT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+        </ul>
+        <div>
+          <button>Create Account</button>
+        </div>
+    </div>
   )
 }
 
 export default Navbar
+
