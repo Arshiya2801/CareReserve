432598f8353619c91d2452141f5c4f5abb5dac10
Arshiya Singh <singharshiya28@gmail.com>
2025-06-23 16:33:11 +0530

    seventh commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div></div>
+    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg-px-20'>
+        {/*left side*/}
+        <div className='md:w-1/2 flex-flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px] '>
+            <p className='text-3xl md:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight mt-6 '>
+                Book Appointment <br /> With Trusted Doctors
+            </p>
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light mt-6'>
+                <img claasName=' w-28' src={assets.group_profiles} alt=""/>
+                <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/>schedule your appointment hassle-free.</p>
+            </div>
+            <a href="#speciality" className='mt-4 flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm hover:scale-105 transition-all duration-300 w-fit'>Book Appointment <img  className=' w-3' src={assets.arrow_icon} alt=""/></a>
+        </div>
+        {/* right side */}
+        <div className='md:w-1/2 relative'>
+            <img className='w-full md:absolute bottom-0 h-auto rounded-1g'
+            src={assets.header_img} alt=""/>
+        </div>
+    </div>
+    
   )
 }
 
