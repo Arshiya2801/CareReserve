a40438abb2eea7ea89a1e0c02d3da20a2aeb550c
Arshiya Singh <singharshiya28@gmail.com>
2025-06-24 22:48:07 +0530

    userdata

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+  const [userData,setUserData] =useState({
+    name: 'Akshat Singh',
+    image:assets.profile_pic,
+    email:'singhakshat@gmail.com',
+    phone:'+91 9382356626',
+    address:{
+      line1:'57th Cross, Sector-5',
+      line2:'New Delhi, India'
+    },
+    gender: 'Male',
+    dob:'2000-01-20'
+  })
+
+  const [isEdit,setIsEdit] =useState(false)
   return (
-    <div></div>
+    <div>
+        <img src={userData.image} alt="" />
+        {
+          isEdit
+          ? <input type="text" />
+          : <p>{userData.name}</p>
+        }
+    </div>
   )
 }
 
